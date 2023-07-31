301b998ad061a47e3048806720446a9352c2e891
1057305798@qq.com <1057305798@qq.com>
2023-07-31 08:24:49 +0800

    登录

---
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -0,0 +1,32 @@
+package com.hmdp.utils;
+
+import com.hmdp.dto.UserDTO;
+import com.hmdp.entity.User;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 创建session对象
+        HttpSession session = request.getSession();
+        // 获取session
+        Object user = session.getAttribute("user");
+        // 判断用户是否存在
+        if (user==null){
+            response.setStatus(401);
+            return false;
+        }
+        // 将用户放入threadlocal
+        UserHolder.saveUser((User) user);
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        UserHolder.removeUser();
+    }
+}
